edd4c1a9488af697b96f569180ba91fe4a19a292
Fikar <fikarnugraha18@gmail.com>
2026-02-26 16:36:10 +0700

    Fix port to use process.env.PORT

--- src/server.ts
+++ src/server.ts
@@ -28,6 +28,7 @@ app.use((req, res) => {
   res.status(404).json({ message: "Route not found" });
 });
 
-app.listen(3333, () => {
-  console.log("Server running on port 3333");
+const PORT = process.env.PORT || 3333;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
